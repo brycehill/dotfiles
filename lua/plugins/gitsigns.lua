e3c4ac7577315b8a87181f261874a0391f18fe87
Bryce Hill <bryceahill@gmail.com>
2024-05-10 13:35:43 -0700

    Remove unused gitsigns stuff

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -13,23 +13,7 @@ return {
 			map("n", "]h", gs.next_hunk, "Next Hunk")
 			map("n", "[h", gs.prev_hunk, "Prev Hunk")
 
-			-- Actions
-			map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
-			map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
-			map("v", "<leader>hs", function()
-				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Stage hunk")
-			map("v", "<leader>hr", function()
-				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Reset hunk")
-
-			map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
-			map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
-
-			map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
-
-			map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
-
+			-- Blame
 			map("n", "<leader>hb", function()
 				gs.blame_line({ full = true })
 			end, "Blame line")
